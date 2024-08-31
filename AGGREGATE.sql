221d88d67bc4f970530a41fef4684b9a641f05db
edugichuki <muriithigichuki854@gmail.com>
2024-08-31 22:11:53 +0300

    data interview

--- AGGREGATE.sql
+++ AGGREGATE.sql
@@ -78,3 +78,32 @@ SELECT count(libname)
 FROM pls_fy2018_libraries;
 
 -- Using having clause to filter the result of an aggregate 
+
+
+-- INSPECTING AND MODIFYING DATA
+SELECT * FROM meat_poultry_egg_establishments;
+
+CREATE INDEX company_idx ON meat_poultry_egg_establishments(company);
+
+SELECT COUNT(*) FROM meat_poultry_egg_establishments;
+
+-- this query tells us where we have the same company listed multiple times at the same address
+-- or rather, the query finds multiple companies at the same address
+SELECT company, street, city, st, count(*) AS address_count
+FROM meat_poultry_egg_establishments
+GROUP BY company, street, city, st
+HAVING count(*) > 1
+ORDER BY company, street, city, st;
+
+-- example 2 - checking for missing values
+-- let's ask some basic questions that helps us write queries that tell meaningful stories
+-- 1. how many of the meat, poultry, and egg processing companies are in each state
+SELECT st, count(*) as st_count
+from meat_poultry_egg_establishments
+group by st
+order by st;
+
+-- using IS NULL to find missing values in the st column
+select establishment_number,company,city,st,zip
+from meat_poultry_egg_establishments
+where st is null;
